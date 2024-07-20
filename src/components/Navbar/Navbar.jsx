eb375ce955dff4076601ad1c5ebe9570aaf30d1a
manasjhamj <jhamanas939@gmail.com>
2024-07-20 23:01:21 +0530

    initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,56 @@
+import { Link, NavLink } from 'react-router-dom'
+import { useState } from 'react'
+
+// import Logo from '../../assets/'
+
+import './navbar.css'
+
+function Navbar() {
+
+
+    const [colorChange, setColorchange] = useState(false);
+    const changeNavbarColor = () => {
+        if (window.scrollY >= 80) {
+            setColorchange(true);
+        } else {
+            setColorchange(false);
+        }
+    };
+    window.addEventListener("scroll", changeNavbarColor);
+
+    return (
+        <nav>
+            <div className={
+                colorChange
+                    ? "navbar colorChange"
+                    : "navbar"
+            }>
+                <div className='logo'>
+                    <Link to='/'>
+                        {/* <img src={Logo} alt="Logo" /> */}
+                    </Link>
+                </div>
+                <div className='links'>
+                    <NavLink to={'/'} className={({ isActive }) => `link
+                        ${isActive ? "active" : "non-active"}`}>
+                        Home
+                    </NavLink>
+                    <NavLink to={'/work'} className={({ isActive }) => `link
+                        ${isActive ? "active" : "non-active"}`}>
+                        Work
+                    </NavLink>
+                    <NavLink to={'/about'} className={({ isActive }) => `link
+                        ${isActive ? "active" : "non-active"}`}>
+                        About
+                    </NavLink>
+                    <NavLink to={'/contact'} className={({ isActive }) => `link
+                        ${isActive ? "active" : "non-active"}`}>
+                        Contact
+                    </NavLink>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
